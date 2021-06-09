88cbbe44811ba84cb14d3289ea320b17c2409fb1
Faxel <74604677+threat0@users.noreply.github.com>
2021-06-09 00:13:18 +0000

    Update m.py

--- m.py
+++ m.py
@@ -18,13 +18,12 @@ def Auto(a):
   sys.stdout.flush()
   time.sleep(ts)
 
-def load(mot):
-    home = ['/', '-', '|']
-    for i in range(5):
-        for x in range(len(home)):
-            sys.stdout.write('\r{}{}'.format(str(mot), home[x]))
-            time.sleep(.3)
-            sys.stdout.flush()
+def hack():
+    hacking = [" ", '.', '..', '...']
+    for h in hacking:
+        sys.stdout.write("\r\033[1;91m[\033[38;5;112m●\033[1;91m] \033[38;5;118mVeuillez patientez nous vérifions s'il y a une mise à jour\033[1;33m" + h)
+        sys.stdout.flush()
+        time.sleep(1)
 
 Dev_faxel =("""
 \033[1;97m╔═════════════════════════════════╗
@@ -35,7 +34,6 @@ def prec():
  f = "\x1b[2K"
  sys.stdout.write(f)
 
-
 def Update():
  _cls();print(Dev_faxel)
  print("\033[1;97m║ \x1b[38;5;115m Veuillez saisir\x1b[38;5;112m O\x1b[38;5;115m ou\x1b[38;5;198m N\x1b[38;5;115m. \x1b[38;5;113mMerci  \033[1;97m║")
@@ -43,16 +41,16 @@ def Update():
  if dmd == "n" or dmd == "N":
   Update()
  elif dmd == "o" or dmd == "O":
-  _cls()
-  load("\033[1;97m Veuillez patientez nous vérifions s'il y a une mise à jour\033[1;93m ")
-  prec();_cls();print(Dev_faxel)
+  _cls();hach();prec()
+  #_cls();
+  print(Dev_faxel)
   os.system('git pull')
   raw_input("\t\033[1;91m[\033[38;5;90m!\033[1;91m]\033[1;9;38;5;95m Hit Enter \033[1;0;1;91m[\033[38;5;9m!\033[1;91m]")
   #os.system("cd $HOME/bash && python3 setup.py ")
-  os.system("clear && python3 setup.py ")
+  os.system("python3 setup.py ")
  else:
   print("\n\x1b[1;91m[\x1b[1;97m+\x1b[1;91m]\x1b[1;97m L'option \x1b[3;5;1;38;5;97m" + dmd + "\x1b[3;0;0;1;97m est indisponible. \x1b[1;91m[\x1b[1;97m+\x1b[1;91m]")
-  time.sleep(3);Update()
+  time.sleep(6);Update()
 ####################################################################################################
 if __name__ == "__main__":
  try:
