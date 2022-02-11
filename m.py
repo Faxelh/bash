9c5d24733060ff0834632fefd4dfb06c72caff3c
Faxel <74604677+threat0@users.noreply.github.com>
2022-02-11 11:49:51 +0000

    Update m.py

--- m.py
+++ m.py
@@ -21,7 +21,7 @@ def Auto(a):
 def hack():
     hacking = [" ", '.', '..', '...']
     for h in hacking:
-        sys.stdout.write("\r\033[1;91m[\033[38;5;112m●\033[1;91m] \033[38;5;118mVeuillez patientez nous vérifions s'il y a une mise à jour\033[1;33m" + h)
+        sys.stdout.write("\r\033[1;91m[\033[38;5;112m●\033[1;91m] \033[38;5;118mPlz wait we check if there is an update\033[1;33m" + h)
         sys.stdout.flush()
         time.sleep(1)
 
@@ -36,8 +36,8 @@ def prec():
 
 def Update():
  _cls();print(Dev_faxel)
- print("\033[1;97m║ \x1b[38;5;115m Veuillez saisir\x1b[38;5;112m O\x1b[38;5;115m ou\x1b[38;5;198m N\x1b[38;5;115m. \x1b[38;5;113mMerci  \033[1;97m║")
- dmd = raw_input("\033[1;97m╚\033[1;31m▶ \033[1;97mVeux-tu verifié s'il y a une mise a jour? \x1b[1;91m: \x1b[38;5;91m")
+ print("\033[1;97m║ \x1b[38;5;115m Please enter\x1b[38;5;112m O\x1b[38;5;115m or\x1b[38;5;198m N\x1b[38;5;115m. \x1b[38;5;113mThank  \033[1;97m║")
+ dmd = raw_input("\033[1;97m╚\033[1;31m▶ \033[1;97mDo you want to check if there is an update? \x1b[1;91m: \x1b[38;5;91m")
  if dmd == "n" or dmd == "N":
   Update()
  elif dmd == "o" or dmd == "O":
@@ -50,7 +50,7 @@ def Update():
   os.system("exit(1)")
   os.system("python3 setup.py ")
  else:
-  print("\n\x1b[1;91m[\x1b[1;97m+\x1b[1;91m]\x1b[1;97m L'option \x1b[3;5;1;38;5;97m" + dmd + "\x1b[3;0;0;1;97m est indisponible. \x1b[1;91m[\x1b[1;97m+\x1b[1;91m]")
+  print("\n\x1b[1;91m[\x1b[1;97m+\x1b[1;91m]\x1b[1;97m The option \x1b[3;5;1;38;5;97m" + dmd + "\x1b[3;0;0;1;97m is unavailable. \x1b[1;91m[\x1b[1;97m+\x1b[1;91m]")
   time.sleep(6);Update()
 ####################################################################################################
 if __name__ == "__main__":
@@ -59,7 +59,7 @@ if __name__ == "__main__":
  except Exception as e:
   x = sys.exc_info()
   print("\033[1;97m" + 35 * "═")
-  print(f"\x1b[1;91m[\x1b[1;97m+\x1b[1;91m]\x1b[1;97m Erreur \x1b[1;91m:\x1b[38;5;95m {x[0].__name__}\n\x1b[1;91m[\x1b[1;97m+\x1b[1;91m]\x1b[1;97m Texte \x1b[1;91m :\x1b[38;5;92m {e}\n\x1b[1;91m[\x1b[1;97m+\x1b[1;91m]\x1b[1;97m Ligne \x1b[1;91m :\x1b[38;5;95m {x[2].tb_lineno}")
-  print("\x1b[38;5;199m Veuillez installé les modules requis puis relancé.")
+  print(f"\x1b[1;91m[\x1b[1;97m+\x1b[1;91m]\x1b[1;97m Error \x1b[1;91m:\x1b[38;5;95m {x[0].__name__}\n\x1b[1;91m[\x1b[1;97m+\x1b[1;91m]\x1b[1;97m Text \x1b[1;91m :\x1b[38;5;92m {e}\n\x1b[1;91m[\x1b[1;97m+\x1b[1;91m]\x1b[1;97m Line \x1b[1;91m :\x1b[38;5;95m {x[2].tb_lineno}")
+  print("\x1b[38;5;199m Please install the required modules and restart.")
   print("\033[1;97m" + 35 * "═")
   time.sleep(6)
